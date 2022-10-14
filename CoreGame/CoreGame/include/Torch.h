3d095288cfc991bd8b3190ddb6665fb9a8f0713b
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2022-10-14 17:49:57 +0200

    Feature: added Torch scene

--- CoreGame/CoreGame/include/Torch.h
+++ CoreGame/CoreGame/include/Torch.h
@@ -8,28 +8,45 @@
 #include "Godot.hpp"
 #include "Light2D.hpp"
 #include "Node2D.hpp"
+#include "Timer.hpp"
+#include <iostream>
+#include <string>
 
 class Torch : public godot::Node2D {
-
   GODOT_CLASS(Torch, godot::Node2D);
-
+  static constexpr float defaultDischargeRate = {1.0f};
   godot::Light2D *m_shadowCaster = nullptr;
   godot::Light2D *m_AmbientLight = nullptr;
-  float m_energy{};
-  float m_intensity{};
-  float m_prev_intensity{};
+  godot::Timer *m_playAnimationTimer = nullptr;
+  godot::Timer *m_stopAnimationTimer = nullptr;
+  godot::Timer *m_blinkTimer = nullptr;
+  godot::Timer *m_stopBlinkTimer = nullptr;
+  float m_energy{1.0f};
+  float m_intensity{1.0f};
+  float m_prev_intensity{1.0f};
+  bool m_playAnimation{};
+  bool m_blink{};
+
+  void get_nodes();
 
 public:
   float magnitude = 1.0f;
   float dischargeRate = 1.0f;
+  Torch() {
+    std::cout << "Light Intensity: " << m_intensity << ", default magnitude: " << magnitude <<  std::endl; }
   static void _register_methods();
   void _init() {}
   void _ready();
-  void _process();
+  void _process(real_t delta);
   void set_energy(float energy);
   [[nodiscard]] float get_energy() const;
   inline void discharge();
   void full_charge();
+  void _on_PlayAnimationTimer_timeout();
+  void _on_BlinkTimer_timeout();
+  void _on_StopAnimationTimer_timeout();
+  void _on_StopBlinkTimer_timeout();
+  void playAnimation(real_t t);
 };
 
 #endif // COREGAME_TORCH_H
--- CoreGame/CoreGame/src/Torch.cpp
+++ CoreGame/CoreGame/src/Torch.cpp
@@ -3,27 +3,48 @@
 //
 
 #include "Torch.h"
+#include "CoreMath.h"
+#include <iostream>
 #include <string>
 
 void Torch::_register_methods() {
   godot::register_property("intensity", &Torch::magnitude, 1.0f);
-  godot::register_property("dischargeRate", &Torch::dischargeRate, 1.0f);
+  godot::register_property("dischargeRate", &Torch::dischargeRate, defaultDischargeRate);
   godot::register_method("_ready", &Torch::_ready);
   godot::register_method("_process", &Torch::_process);
+  godot::register_method("_on_PlayAnimationTimer_timeout", &Torch::_on_PlayAnimationTimer_timeout);
+  godot::register_method("_on_BlinkTimer_timeout", &Torch::_on_BlinkTimer_timeout);
+  godot::register_method("_on_StopAnimationTimer_timeout", &Torch::_on_StopAnimationTimer_timeout);
+  godot::register_method("_on_StopBlinkTimer_timeout", &Torch::_on_StopBlinkTimer_timeout);
+}
+
+void Torch::get_nodes() {
+  m_shadowCaster = get_node<godot::Light2D>("ShadowCaster");
+  m_AmbientLight = get_node<godot::Light2D>("AmbientLight");
+  m_playAnimationTimer = get_node<godot::Timer>("PlayAnimationTimer");
+  m_stopAnimationTimer = get_node<godot::Timer>("StopAnimationTimer");
+  m_blinkTimer = get_node<godot::Timer>("BlinkTimer");
+  m_stopBlinkTimer = get_node<godot::Timer>("StopBlinkTimer");
 }
 
 void Torch::_ready() {
+  get_nodes();
   full_charge();
-  m_shadowCaster = get_node<godot::Light2D>("ShadowCaster");
-  m_AmbientLight = get_node<godot::Light2D>("AmbientLight");
   set_energy(magnitude * m_intensity);
-  std::string m = "Torch ready. Light energy: " + std::to_string(get_energy());
-  m += ", discharge rate: " + std::to_string(dischargeRate);
-  godot::Godot::print(m.c_str());
+  std::cout << "Torch ready. Light magnitude: " << magnitude << ", energy: " << get_energy()
+            << ", discharge rate: " << dischargeRate << std::endl;
 }
 
-void Torch::_process() {
+void Torch::_process(real_t delta) {
   discharge();
+  if (m_intensity <= 0.5f && !m_playAnimation)
+    m_stopAnimationTimer->start();
+
+  if (m_blink) {
+    static real_t t{0};
+    t += delta;
+    playAnimation(t / m_stopBlinkTimer->get_wait_time());
+  }
   if (m_intensity <= 0.25f) full_charge();
 }
 
@@ -34,6 +55,7 @@ void Torch::set_energy(float energy) {
     m_AmbientLight->set_energy(m_energy);
   }
 }
+
 float Torch::get_energy() const { return m_energy; }
 
 void Torch::discharge() {
@@ -44,10 +66,9 @@ void Torch::discharge() {
   }
 
   set_energy(magnitude * m_intensity);
+
   if (m_prev_intensity - m_intensity >= 0.05) {
-    auto m = "Light intensity: " + std::to_string(m_intensity);
-    m += ", energy: " + std::to_string(get_energy());
-    godot::Godot::print(m.c_str());
+    std::cout << "Light intensity: " << m_intensity << ", energy: " << get_energy() << std::endl;
     m_prev_intensity = m_intensity;
   }
 }
@@ -55,4 +76,37 @@ void Torch::discharge() {
 void Torch::full_charge() {
   m_intensity = 1.0f;
   m_prev_intensity = 1.0f;
+}
+
+void Torch::_on_PlayAnimationTimer_timeout() {
+#ifdef DEBUG
+  std::cout << "Start Animation Timer" << std::endl;
+#endif
+  m_playAnimation = true;
+  m_blinkTimer->start();
+  m_stopAnimationTimer->start();
+}
+
+void Torch::_on_BlinkTimer_timeout() {
+  m_blink = true;
+  m_stopBlinkTimer->start();
+}
+
+void Torch::_on_StopAnimationTimer_timeout() {
+#ifdef DEBUG
+  std::cout << "Stop animation timer" << std::endl;
+#endif
+  m_playAnimation = false;
+  if (!m_blinkTimer->is_stopped()) m_blinkTimer->stop();
+  m_stopBlinkTimer->start();
+}
+
+void Torch::_on_StopBlinkTimer_timeout() {
+  m_blink = false;
+  m_blinkTimer->start();
+}
+
+void Torch::playAnimation(real_t t) {
+  auto intensity = 1.0f - 0.5f * CoreGame::crossfade(CoreGame::smoothStep3(t), 1 - CoreGame::smoothStep3(t), t);
+  set_energy(magnitude * intensity);
 }
