7b28e3c3f2559d237d25bd9e27d1ad0e901599f4
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2022-10-20 13:26:14 +0200

    Refactor: cleaner Main scene and better scenes organization

--- CoreGame/CoreGame/include/Main.h
+++ CoreGame/CoreGame/include/Main.h
@@ -5,29 +5,15 @@
 #ifndef COREGAME_MAIN_H
 #define COREGAME_MAIN_H
 
-#include "Map.h"
-#include "Player.h"
-#include "VisibilityPolygon.h"
-#include <ColorRect.hpp>
+#include <CanvasModulate.hpp>
 #include <Godot.hpp>
 #include <Node2D.hpp>
-#include <PackedScene.hpp>
-#include <ResourceLoader.hpp>
-#include <Viewport.hpp>
 
 class Main : public godot::Node2D {
   GODOT_CLASS(Main, godot::Node2D)
-
-  Player *m_player;
-  Map *m_map;
-
-    VisibilityPolygon *m_visibility_polygon;
-    godot::ColorRect *m_lightFilter;
-    godot::Vector2 m_screen_size;
+  godot::CanvasModulate *m_darknessLayer = nullptr;
 
 public:
-    float lightFalloff = 5.0f;
-   // ~Main() {m_visibility_polygon->queue_free();}
   void _init() {}
   void _ready();
   void _process();
--- CoreGame/CoreGame/src/Main.cpp
+++ CoreGame/CoreGame/src/Main.cpp
@@ -5,35 +5,14 @@
 #include "Main.h"
 
 void Main::_ready() {
-  m_screen_size = get_viewport_rect().get_size();
-  m_player = get_node<Player>("Player");
-  m_map = get_node<Map>("Map");
-  m_visibility_polygon = get_node<VisibilityPolygon>("VisibilityNode");
-  m_lightFilter = get_node<godot::ColorRect>("LightFilter");
-
-  godot::Ref<godot::ShaderMaterial> lightFilter_mat = m_lightFilter->get_material();
-  lightFilter_mat->set_shader_param("fall_off", lightFalloff);
-  lightFilter_mat->set_shader_param("aspect_ratio", m_screen_size.y / m_screen_size.x);
-  m_visibility_polygon->set_player(m_player);
-  m_visibility_polygon->set_walls(m_map->get_walls());
-  m_visibility_polygon->set_light_falloff(lightFalloff);
+  m_darknessLayer = get_node<godot::CanvasModulate>("Darkness");
+  if (!m_darknessLayer->is_visible()) m_darknessLayer->set_visible(true);
   godot::Godot::print("Main scene ready");
 }
 
-void Main::_process() {
-  // Upload mouse normalized coordinates to shader
-  auto playerPosition = m_player->get_position();
-  auto playerDirection = m_player->get_direction();
-  playerPosition.x /= m_screen_size.x;
-  playerPosition.y /= m_screen_size.y;
-
-  godot::Ref<godot::ShaderMaterial> lightFilter_mat = m_lightFilter->get_material();
-  lightFilter_mat->set_shader_param("light_position", playerPosition);
-  lightFilter_mat->set_shader_param("heading", playerDirection);
-}
+void Main::_process() {}
 
 void Main::_register_methods() {
   godot::register_method("_ready", &Main::_ready);
   godot::register_method("_process", &Main::_process);
-  godot::register_property("_lightFalloff", &Main::lightFalloff, 1.0f);
 }
